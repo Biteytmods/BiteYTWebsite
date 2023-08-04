c96fcde6163f7e18ec6efca7b35cdf7a92f81dd5
CinarKK9 <cinarkk9@gmail.com>
2023-08-04 09:27:04 +0300

    Improvements

--- index.js
+++ index.js
@@ -17,19 +17,21 @@ elementsToObserve.forEach((element) => {
 });
 
 //----------------------------------------------------------------------------------------
+let clickCount = 0;
 
 document.addEventListener('DOMContentLoaded', function() {
   const themeSwitch = document.getElementById('themeSwitch');
   const lightElements = document.querySelector("html");
   const logo = document.querySelector('.switch-logo');
 
-  themeSwitch.addEventListener('change', function() {
-    if (this.checked) {
+  themeSwitch.addEventListener('click', function() {
+    clickCount++;
+    if (clickCount % 2 == Math.abs(1)) {
       lightElements.setAttribute('data-bs-theme', 'dark');
-      logo.style.background = 'url(images/moon.svg)';
+      logo.style.background = 'url(images/sun.svg)';
     } else {
       lightElements.setAttribute('data-bs-theme', 'light');
-      logo.style.background = 'url(images/sun.svg)';
+      logo.style.background = 'url(images/moon.svg)';
     }
   });
 });
